44a6eb174cb84cd0410341d5f48d4c1de44d4b9d
luizmarcello <luizmarcello.vmo@hotmail.com>
2020-12-11 17:13:34 -0300

    11.12.2020-17:13

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,85 +13,87 @@
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1;shrink-to-fit=no" />
 
-    <style>
+    {{-- <style>
         .destaque {
             background-color: yellow;
             border: solid 1px black;
         }
 
-    </style>
+    </style> --}}
+
 </head>
-{{-- breakpoints do Bootstrap
-- xs (extra-small devices) - <576px - sm (small devices) - 576px <=vwp <=767px - md (medium devices) - 768px <=vwp
-    <=991px - lg (large devices) - 992px <=vwp <=1999px - xg (extra-large devices) - vwp=> 1200px
-    --}}
 
-    <body>
-        <div class="container destaque">
-            <h3>Grid System</h3>
-            <div class="row destaque">
-                <div class="col destaque">
-                    parte 1
-                </div>
-                <div class="col destaque">
-                    parte 2
-                </div>
-                <div class="col destaque">
-                    parte 3
-                </div>
-                <div class="col destaque">
-                    parte 4
+<body>
+    <div class="container">
+
+        <div class="row">
+            <div class="col-8">
+                <div class="jumbotron">
+                    <h1 class="display-4">Bentley Brasil</h1>
+                    <hr class="my-4">
+                    <h3>
+                        <p>Internet Rural via Satélite.</p>
+                    </h3>
+                    <a class="btn btn-primary btn-lg" href="#" role="button">Saiba mais</a>
                 </div>
             </div>
 
-            <div class="row destaque">
-                <div class="col destaque">
-                    parte 1
-                </div>
-                <div class="col destaque">
-                    parte 2
-                </div>
-                <div class="col destaque">
-                    <input type="text" style="width: 100%">
-                </div>
-            </div>
+        </div>
 
-            <div class="row destaque">
-                <div class="col-md-3 col-sm-12 destaque">
-                    <input type="text" style="width: 100%">
-                </div>
-                <div class="col-md-6 col-sm-12 destaque">
-                    <input type="text" style="width: 100%">
-                </div>
-                <div class="col-md-3 col-sm-12 destaque">
-                    <input type="text" style="width: 100%">
-                </div>
-            </div>
+        <div class="row">
+            <div class="col-12">
 
-            <div class="row destaque">
-                <div class="col-md-3 col-sm-4 col-12 destaque">
-                    parte 1
+                <div>
+                    <img src="{{ 'img/landsat8.jpg' }}" class="img-fluid" alt="Responsive image" width="90%"
+                        height="90%">
                 </div>
-                <div class="col-md-6 col-sm-4 col-12 destaque">
-                    parte 2
-                </div>
-                <div class="col-md-3 col-sm-4 col-12 destaque">
-                    parte 3
+
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-6">
+                <div class="jumbotron jumbotron-fluid">
+                    <h1 class="display-4">Controle e Gerenciamento</h1>
+                    <p class="lead"> </p>
+                    <p class="lead"> </p>
                 </div>
             </div>
         </div>
 
+        <div class="row">
+            <div class="col-12">
+                <div>
+                    <img src="{{ 'img/logobentley.png' }}" class="img-fluid" alt="Responsive image">
+                </div>
+            </div>
+        </div>
 
-        <!--JavaScript at end of body for optimized loading-->
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-        </script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-            integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
-        </script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
-            integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous">
-        </script>
-    </body>
+    </div>
+
+    <!--JavaScript at end of body for optimized loading-->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
+        integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous">
+    </script>
+</body>
 
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
