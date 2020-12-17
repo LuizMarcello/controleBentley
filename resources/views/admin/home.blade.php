604212aedafab0eaec3ee0ff39bce3b4bff03544
luizmarcello <luizmarcello.vmo@hotmail.com>
2020-12-17 09:46:21 -0300

    17.12.2020_9:46hs

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -6,7 +6,8 @@
 
     {{-- importando css do bootstrap --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
+        crossorigin="anonymous">
 
     <meta charset="utf-8">
 
@@ -21,64 +22,67 @@
 </head>
 
 <body>
-    {{-- tirado o fixed-top na nav abaixo --}}
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-        <a class="navbar-brand" href="#">
-            <img src="{{ 'img/logobentley.png' }}" alt="Responsive image" width="190" height="60"
-                class="d-inline-block align-center"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudo-navbar"
-            aria-controls="conteudo-navbar" aria-expanded="false" aria-label="Botão para expansão do menu">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        {{-- collapse: Que pode sumir, dependendo os breakpoints(ex: resolução sm)
+    <div class="container">
+{{-- tirado o fixed-top na nav abaixo --}}
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <a class="navbar-brand" href="#">
+        <img src="{{ 'img/logobentley.png' }}" alt="Responsive image" width="190" height="60"
+            class="d-inline-block align-center"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudo-navbar"
+        aria-controls="conteudo-navbar" aria-expanded="false" aria-label="Botão para expansão do menu">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    {{-- collapse: Que pode sumir, dependendo os breakpoints(ex: resolução sm)
+    --}}
+    <div class="collapse navbar-collapse" id="conteudo-navbar">
+        {{-- mr: helper css de alinhamento flexbox, para a direita do elemento
+        mais
+        próximo --}}
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="/">Home</a>
+            </li>
+            {{-- Menu filho - Sub menu dropdown--}}
+            <li class="nav-item dropdown">
+                {{-- role: indica que não é um link normal, mas é um botão para
+                rolagem(perfil de botão) --}}
+                {{-- Html5 não tem um atributo toggle: Data- api: Maneira de
+                passar
+                meta informações para os elementos html,
+                mesmo que eles não possuam aquela meta-informação --}}
+                <a class="nav-link dropdown-toggle" href="#" id="navbar-dropdown" role="button"
+                    data-toggle="dropdown">
+                    Sistema
+                </a>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="admin/clientes">Clientes</a>
+                    <a class="dropdown-item" href="admin/distribuidores">Distribuidores</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="admin/designacao">Designação</a>
+                    <a class="dropdown-item" href="admin/modens">modens</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="admin/tecnicos">Tecnicos</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Configurações</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Sair</a>
+            </li>
+        </ul>
+        {{-- my: m:Margim e y:top e buttom, no form
         --}}
-        <div class="collapse navbar-collapse" id="conteudo-navbar">
-            {{-- mr: helper css de alinhamento flexbox, para a direita do elemento
-            mais
-            próximo --}}
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="/">Home</a>
-                </li>
-                {{-- Menu filho - Sub menu dropdown--}}
-                <li class="nav-item dropdown">
-                    {{-- role: indica que não é um link normal, mas é um botão para
-                    rolagem(perfil de botão) --}}
-                    {{-- Html5 não tem um atributo toggle: Data- api: Maneira de
-                    passar
-                    meta informações para os elementos html,
-                    mesmo que eles não possuam aquela meta-informação --}}
-                    <a class="nav-link dropdown-toggle" href="#" id="navbar-dropdown" role="button"
-                        data-toggle="dropdown">
-                        Sistema
-                    </a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="admin/clientes">Clientes</a>
-                        <a class="dropdown-item" href="admin/distribuidores">Distribuidores</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="admin/designacao">Designação</a>
-                        <a class="dropdown-item" href="admin/modens">modens</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="admin/tecnicos">Tecnicos</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Configurações</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Sair</a>
-                </li>
-            </ul>
-            {{-- my: m:Margim e y:top e buttom, no form
-            --}}
-            <form class="form-inline my-2">
-                <input class="form-control" type="search" placeholder="Pesquisa...">
-                <button class="btn btn-outline-success ml-2" type="submit">Pesquisar</button>
-            </form>
-        </div>
-    </nav>
+        <form class="form-inline my-2">
+            <input class="form-control" type="search" placeholder="Pesquisa...">
+            <button class="btn btn-outline-success ml-2" type="submit">Pesquisar</button>
+        </form>
+    </div>
+</nav>
 
-    <h3>Esta é a view home</h3>
+<h3>Esta é a view home</h3>
+    </div>
+    
 
     <!--JavaScript at end of body for optimized loading-->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
